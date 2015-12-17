1af09fcfb794eb622f76c6b95da96a735c97d1e5
lgelb <lucy.gelb@gmail.com>
2015-12-17 11:59:58 -0700

    forcings update

---
+++ MACAdata/macav1metdata_curl.sh
@@ -0,0 +1,55 @@
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_pr_CanESM2_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=precipitation_flux&variable-name=pr
+" -O agg_macav1metdata_pr_CanESM2_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_tasmin_CanESM2_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=air_temperature&variable-name=tasmin
+" -O agg_macav1metdata_tasmin_CanESM2_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_tasmax_CanESM2_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=air_temperature&variable-name=tasmax
+" -O agg_macav1metdata_tasmax_CanESM2_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_pr_CCSM4_r6i1p1_rcp45_2006_2100_WUSA.nc&variable=precipitation_flux&variable-name=pr
+" -O agg_macav1metdata_pr_CCSM4_r6i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_tasmin_CCSM4_r6i1p1_rcp45_2006_2100_WUSA.nc&variable=air_temperature&variable-name=tasmin
+" -O agg_macav1metdata_tasmin_CCSM4_r6i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_tasmax_CCSM4_r6i1p1_rcp45_2006_2100_WUSA.nc&variable=air_temperature&variable-name=tasmax
+" -O agg_macav1metdata_tasmax_CCSM4_r6i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_pr_CSIRO-Mk3-6-0_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=precipitation_flux&variable-name=pr
+" -O agg_macav1metdata_pr_CSIRO-Mk3-6-0_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_tasmin_CSIRO-Mk3-6-0_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=air_temperature&variable-name=tasmin
+" -O agg_macav1metdata_tasmin_CSIRO-Mk3-6-0_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_tasmax_CSIRO-Mk3-6-0_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=air_temperature&variable-name=tasmax
+" -O agg_macav1metdata_tasmax_CSIRO-Mk3-6-0_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_pr_HadGEM2-CC365_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=precipitation_flux&variable-name=pr
+" -O agg_macav1metdata_pr_HadGEM2-CC365_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_tasmin_HadGEM2-CC365_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=air_temperature&variable-name=tasmin
+" -O agg_macav1metdata_tasmin_HadGEM2-CC365_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_tasmax_HadGEM2-CC365_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=air_temperature&variable-name=tasmax
+" -O agg_macav1metdata_tasmax_HadGEM2-CC365_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_pr_HadGEM2-ES365_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=precipitation_flux&variable-name=pr
+" -O agg_macav1metdata_pr_HadGEM2-ES365_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_tasmin_HadGEM2-ES365_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=air_temperature&variable-name=tasmin
+" -O agg_macav1metdata_tasmin_HadGEM2-ES365_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_tasmax_HadGEM2-ES365_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=air_temperature&variable-name=tasmax
+" -O agg_macav1metdata_tasmax_HadGEM2-ES365_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_pr_inmcm4_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=precipitation_flux&variable-name=pr
+" -O agg_macav1metdata_pr_inmcm4_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_tasmin_inmcm4_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=air_temperature&variable-name=tasmin
+" -O agg_macav1metdata_tasmin_inmcm4_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_tasmax_inmcm4_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=air_temperature&variable-name=tasmax
+" -O agg_macav1metdata_tasmax_inmcm4_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_pr_IPSL-CM5B-LR_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=precipitation_flux&variable-name=pr
+" -O agg_macav1metdata_pr_IPSL-CM5B-LR_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_tasmin_IPSL-CM5B-LR_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=air_temperature&variable-name=tasmin
+" -O agg_macav1metdata_tasmin_IPSL-CM5B-LR_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_tasmax_IPSL-CM5B-LR_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=air_temperature&variable-name=tasmax
+" -O agg_macav1metdata_tasmax_IPSL-CM5B-LR_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_pr_MIROC-ESM_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=precipitation_flux&variable-name=pr
+" -O agg_macav1metdata_pr_MIROC-ESM_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_tasmin_MIROC-ESM_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=air_temperature&variable-name=tasmin
+" -O agg_macav1metdata_tasmin_MIROC-ESM_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_tasmax_MIROC-ESM_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=air_temperature&variable-name=tasmax
+" -O agg_macav1metdata_tasmax_MIROC-ESM_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_pr_MIROC-ESM-CHEM_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=precipitation_flux&variable-name=pr
+" -O agg_macav1metdata_pr_MIROC-ESM-CHEM_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_tasmin_MIROC-ESM-CHEM_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=air_temperature&variable-name=tasmin
+" -O agg_macav1metdata_tasmin_MIROC-ESM-CHEM_r1i1p1_rcp45_2006_2100_WUSA.nc 
+curl  "http://climate.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&lat=43.73019&lon=-116.140143&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_tasmax_MIROC-ESM-CHEM_r1i1p1_rcp45_2006_2100_WUSA.nc&variable=air_temperature&variable-name=tasmax
+" -O agg_macav1metdata_tasmax_MIROC-ESM-CHEM_r1i1p1_rcp45_2006_2100_WUSA.nc 
+
